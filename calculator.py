f25a91c840507818857c14ad74155c99591c7b54
Atabey Aydı <64842894+Libldeaera@users.noreply.github.com>
2022-01-09 22:39:50 +0300

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,21 @@
+Calculation = input("Entry operation: ")
+num1 = int(input("Number1: "))
+num2 = int(input("Number2: "))
+
+if Calculation == "+":
+    result = int(num1) + int(num2)
+    printf("Result: ", str(result))
+    
+    elif  Calculation == "-":
+    result = int(num1) - int(num2)
+    printf("Result: ", str(result))
+    
+    elif  Calculation == "*":
+    result = int(num1) * int(num2)
+    printf("Result: ", str(result))
+    
+    elif  Calculation == "/":
+    result = int(num1) / int(num2)
+    printf("Result: ", str(result))
+    
+  
